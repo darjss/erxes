dc9b738f2909a4fae0bdbe96b2d63a6f3c05460b
batmnkh2344 <batmnkh2344@gmail.com>
2025-11-22 01:58:07 +0800

    chore: use modifierMiddleware for btk routes

--- backend/plugins/btkadmin_api/src/routes/index.ts
+++ backend/plugins/btkadmin_api/src/routes/index.ts
@@ -1,18 +1,18 @@
 import { router as attachmentRoutes } from '@/attachment/routes';
-import { router as companyRoutes } from '~/modules/company/routes';
 import { router as documentRoutes } from '@/document/routes';
-import { router as newsRoutes } from '~/modules/news/routes';
+import { modifierMiddleware } from '~/middlewares/modifierMiddleware';
 import { validator as validationMiddleware } from '~/middlewares/validationMiddleware';
+import { router as companyRoutes } from '~/modules/company/routes';
+import { router as newsRoutes } from '~/modules/news/routes';
 
 import { Router } from 'express';
 
 const router: Router = Router();
 
-router.use('/api', validationMiddleware, [
-  attachmentRoutes,
-  companyRoutes,
-  documentRoutes,
-  newsRoutes,
-]);
+router.use(
+  '/api',
+  [validationMiddleware, modifierMiddleware],
+  [attachmentRoutes, companyRoutes, documentRoutes, newsRoutes],
+);
 
 export { router };
