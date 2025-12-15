29fee134eab560afeff3df561f9d8a4f614a7861
Khe <baterdne.k@gmail.com>
2025-12-15 12:05:52 +0800

    fix: update refetchQueries in useClientsCreate and useMarketsCreate hooks to use correct GraphQL query constants

--- frontend/private-plugins/blocktest_ui/src/modules/markets/hooks/useMarketsCreate.ts
+++ frontend/private-plugins/blocktest_ui/src/modules/markets/hooks/useMarketsCreate.ts
@@ -1,11 +1,11 @@
 import { useMutation } from '@apollo/client';
 import { CREATE_CV_MARKET } from '../graphql/cvMarketsMutation';
+import { GET_CV_MARKETS } from '../graphql/cvMarketsQueries';
 
 export const useMarketsCreate = () => {
   const [createMarket, { loading }] = useMutation(CREATE_CV_MARKET, {
-    refetchQueries: ['GetCVMarkets'],
+    refetchQueries: [GET_CV_MARKETS],
   });
 
   return { createMarket, loading };
 };
-
