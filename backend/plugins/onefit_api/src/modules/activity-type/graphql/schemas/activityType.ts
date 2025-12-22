d4afd39e026638d0d0364764b353f7ac15e3fc71
orshih <orshih943@gmail.com>
2025-12-22 16:59:16 +0800

    chore: save

--- backend/plugins/onefit_api/src/modules/activity-type/graphql/schemas/activityType.ts
+++ backend/plugins/onefit_api/src/modules/activity-type/graphql/schemas/activityType.ts
@@ -39,6 +39,30 @@ export const types = `
     pageInfo: PageInfo
     totalCount: Int
   }
+
+  type OneFitActivityTypeWithAvailability {
+    _id: String
+    createdAt: Date
+    modifiedAt: Date
+    providerId: String
+    provider: OneFitProvider
+    name: OneFitMultilingualString
+    description: OneFitMultilingualStringOptional
+    creditCost: Float
+    duration: Int
+    genderRestriction: OneFitGenderRestriction
+    categoryIds: [String]
+    categories: [OneFitActivityCategory]
+    isActive: Boolean
+    cancellationDeadline: Int
+    availability: OneFitDayAvailability
+  }
+
+  type OneFitActivityTypeWithAvailabilityListResponse {
+    list: [OneFitActivityTypeWithAvailability]
+    pageInfo: PageInfo
+    totalCount: Int
+  }
   input OneFitMultilingualStringInput {
     en: String!
     mn: String!
@@ -62,6 +86,7 @@ export const queries = `
   oneFitActivityTypes(${queryParams}, ${GQL_CURSOR_PARAM_DEFS}): OneFitActivityTypeListResponse
   oneFitActivityTypesCount(${queryParams}): Int
   oneFitActivityType(_id: String): OneFitActivityType
+  oneFitActivityTypesWithAvailability(date: Date!, categoryIds: [String], providerId: String, isActive: Boolean, hasSchedule: Boolean, isFull: Boolean, ${GQL_CURSOR_PARAM_DEFS}): OneFitActivityTypeWithAvailabilityListResponse
 `;
 
 const mutationParams = `
