d4d3717ff81be7f3512eb1a80d0034ee722c6030
orshih <orshih943@gmail.com>
2026-02-09 14:33:59 +0800

    map

--- frontend/private-plugins/onefit_ui/rspack.config.ts
+++ frontend/private-plugins/onefit_ui/rspack.config.ts
@@ -1,5 +1,6 @@
 import { composePlugins, withNx, withReact } from '@nx/rspack';
 import { withModuleFederation } from '@nx/rspack/module-federation';
+import { DefinePlugin } from '@rspack/core';
 
 import baseConfig from './module-federation.config';
 
@@ -20,5 +21,16 @@ export default composePlugins(
   withNx(),
   withReact(),
   withModuleFederation(config, { dts: false }),
+  (config: any) => {
+    config.plugins = config.plugins ?? [];
+    config.plugins.push(
+      new DefinePlugin({
+        'process.env.REACT_APP_GOOGLE_MAP_API_KEY': JSON.stringify(
+          process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+        ),
+      }),
+    );
+    return config;
+  },
 );
 
