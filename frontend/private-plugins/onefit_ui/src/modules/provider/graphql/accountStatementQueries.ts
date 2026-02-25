a23a1292fb20f2e374265c2a9fb3892d20586800
orshih <orshih943@gmail.com>
2026-02-25 08:05:34 +0800

    chore: save

---
+++ frontend/private-plugins/onefit_ui/src/modules/provider/graphql/accountStatementQueries.ts
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const ONE_FIT_ACCOUNT_STATEMENT = gql`
+  query OneFitAccountStatement(
+    $providerId: String
+    $startDate: Date!
+    $endDate: Date!
+  ) {
+    oneFitAccountStatement(
+      providerId: $providerId
+      startDate: $startDate
+      endDate: $endDate
+    ) {
+      rows {
+        year
+        month
+        providerId
+        provider {
+          _id
+          businessName {
+            en
+            mn
+          }
+        }
+        creditsEarnedCompleted
+        creditsEarnedNoShow
+        bookingCountCompleted
+        bookingCountNoShow
+      }
+      totalCreditsEarned
+    }
+  }
+`;
