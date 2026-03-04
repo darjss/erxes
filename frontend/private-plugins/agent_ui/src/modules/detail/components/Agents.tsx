8dbd1e3e8070bcbdb5d88554edd1fc50b6bdeae2
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com.>
2026-03-04 13:53:33 +0800

    remove agents

--- frontend/private-plugins/agent_ui/src/modules/detail/components/Agents.tsx
+++ frontend/private-plugins/agent_ui/src/modules/detail/components/Agents.tsx
@@ -1,4 +1,3 @@
-import { IconSparkles } from '@tabler/icons-react';
 import { useAgentsList } from '../hooks/useAgentsList';
 import { ScrollArea, Spinner } from 'erxes-ui';
 import { AgentItem } from './AgentItem';
@@ -17,19 +16,6 @@ export const Agents = ({
     id: 'main',
     identity: null,
   };
-
-  if (!loading) {
-    return (
-      <div className="h-full w-full flex flex-col gap-4 justify-center items-center text-accent-foreground p-4">
-        <div className="border border-dashed p-6 bg-sidebar rounded-xl">
-          <IconSparkles className="size-8 text-muted-foreground" />
-        </div>
-        <span className="text-sm text-center">
-          No agents yet. Add an agent to get started.
-        </span>
-      </div>
-    );
-  }
 
   return (
     <ScrollArea.Root className="w-full h-full overflow-hidden relative bg-sidebar">
