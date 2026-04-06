4cc2486902e6ea5f4da99892fbd123cc212f16b3
batmnkh2344 <batmnkh2344@gmail.com>
2026-04-06 13:10:23 +0800

    refactor: update oppty hooks to use named GraphQL queries for refetching

--- frontend/private-plugins/block_ui/src/modules/oppty/hooks/useCreateOppty.tsx
+++ frontend/private-plugins/block_ui/src/modules/oppty/hooks/useCreateOppty.tsx
@@ -1,12 +1,14 @@
 import { useMutation } from '@apollo/client';
 import { CREATE_OPPTY_MUTATION } from '@/oppty/graphql/mutations/createOppty';
 import { useToast } from 'erxes-ui';
+import { GET_OPPTYS } from '@/oppty/graphql/queries/getOpptys';
 
 export const useCreateOppty = () => {
   const { toast } = useToast();
   const [createOpptyMutation, { loading, error }] = useMutation(
     CREATE_OPPTY_MUTATION,
     {
+      refetchQueries: [GET_OPPTYS],
       onCompleted: () => {
         toast({
           title: 'Success',
--- frontend/private-plugins/block_ui/src/modules/oppty/hooks/useDeleteOppty.tsx
+++ frontend/private-plugins/block_ui/src/modules/oppty/hooks/useDeleteOppty.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { DELETE_OPPTY_MUTATION } from '@/oppty/graphql/mutations/deleteOppty';
+import { GET_OPPTYS } from '@/oppty/graphql/queries/getOpptys';
 
 export const useDeleteOppty = () => {
   const [removeOppty, { loading }] = useMutation(DELETE_OPPTY_MUTATION, {
-    refetchQueries: ['BlockGetOpptys'],
+    refetchQueries: [GET_OPPTYS],
   });
 
   return { removeOppty, loading };
