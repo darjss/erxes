7a87a66320ce7fde0430a50a5fa8eb8ad8d43e2c
batmnkh2344 <batmnkh2344@gmail.com>
2026-05-07 11:41:55 +0800

    feat: add mushop customer invoice GraphQL queries
    - add mushopMyInvoices and mushopMyInvoiceDetail resolvers for client portal
    - wire invoice queries into mushop_api GraphQL query exports
    - add invoice schema definitions for Mushop customer invoices and transactions

--- backend/plugins/mushop_api/src/apollo/resolvers/queries.ts
+++ backend/plugins/mushop_api/src/apollo/resolvers/queries.ts
@@ -2,10 +2,12 @@ import { supplierQueries } from '@/supplier/graphql/resolvers/queries/supplier';
 import { productQueries } from '@/product/graphql/resolvers/queries/product';
 import { subscriptionQueries } from '@/subscription/graphql/resolvers/queries/mushopSubscription';
 import { subscriptionPlanQueries } from '@/subscription/graphql/resolvers/queries/mushopSubscriptionPlan';
+import { mushopInvoiceQueries } from '@/subscription/graphql/resolvers/queries/invoices';
 
 export const queries = {
   ...supplierQueries,
   ...productQueries,
   ...subscriptionQueries,
   ...subscriptionPlanQueries,
+  ...mushopInvoiceQueries,
 };
