885f7f62269ee989b12e7266432dfe52f907b752
batmnkh2344 <batmnkh2344@gmail.com>
2026-05-14 09:46:14 +0800

    feat(mushop): filter posclient products by supplier id

---
+++ backend/plugins/mushop_api/src/modules/product/beforeResolvers.ts
@@ -0,0 +1,50 @@
+import { BeforeResolverParams } from 'erxes-api-shared/utils';
+import { generateModels } from '~/connectionResolvers';
+
+const resolvers = {
+  posclient: ['cpPoscProducts'],
+};
+
+export const beforeResolverHandlers = async (
+  subdomain: string,
+  params: BeforeResolverParams,
+) => {
+  const { resolver, args, headers } = params;
+
+  if (!resolvers.posclient.includes(resolver)) {
+    return args;
+  }
+
+  console.log('resolver', resolver);
+
+  const supplierId = headers?.['erxes-supplier-id'];
+
+  console.log('supplierId', supplierId);
+
+  if (!supplierId) {
+    return args;
+  }
+
+  const models = await generateModels(subdomain);
+
+  const supplier = await models.Supplier.getSupplier(supplierId);
+
+  if (!supplier?.subdomain) {
+    return args;
+  }
+
+  const products = await models.MushopProduct.find({
+    subdomain: supplier.subdomain,
+  })
+    .distinct('_id')
+    .lean();
+
+  const productIds = new Array(...new Set(products.map((p) => p.toString())));
+
+  return { ...args, ids: productIds.length ? productIds : [] };
+};
+
+export default {
+  resolvers,
+  handler: beforeResolverHandlers,
+};
